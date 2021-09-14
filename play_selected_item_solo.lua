f6a5649d4793324263f1b7b5243e568c398ef7a7
johnloser <john-masterchief@hotmail.com>
2021-09-14 14:38:43 +0800

    Change start_time to beginning of all selected items

--- play_selected_item_solo.lua
+++ play_selected_item_solo.lua
@@ -1,43 +1,57 @@
 
-local item_count = reaper.CountSelectedMediaItems(0)
+function main ()
+    local item_count = reaper.CountSelectedMediaItems(0)
 
-if item_count > 0 then
-    reaper.SoloAllTracks(0)
+    if item_count > 0 then
+        reaper.SoloAllTracks(0)
 
-    local end_time = -1
+        local start_time = -1
+        local end_time = -1
 
-    for i = 0, item_count - 1 do
-        local item = reaper.GetSelectedMediaItem(0, i)
-        local track = reaper.GetMediaItem_Track(item)
-        local  _,track_name = reaper.GetTrackName(track)
+        for i = 0, item_count - 1 do
+            local item = reaper.GetSelectedMediaItem(0, i)
+            local track = reaper.GetMediaItem_Track(item)
 
-        if i == 0 then
-            -- set cursor to the beginning of first selected item
-            reaper.Main_OnCommand(41173, 1)
+            local item_start_time = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+            local item_end_time = item_start_time + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+
+            -- Set Track Solo
+            reaper.SetMediaTrackInfo_Value(track, 'I_SOLO', 1)
+            
+
+            -- Set Start Time
+            if i == 0 then
+                start_time = item_start_time
+            elseif item_start_time < start_time then
+                start_time = item_start_time
+            end
+            
+
+            -- Set End Time
+            if item_end_time > end_time then
+                end_time = item_end_time
+            end
         end
 
-        if i == item_count -1 then
-            end_time = reaper.GetMediaItemInfo_Value(item, "D_POSITION") + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+        -- Move Edit Cursor and Play
+        reaper.MoveEditCursor(start_time - reaper.GetCursorPosition(), false)
+        reaper.OnPlayButton()
+
+
+        -- Defer Loop Until Playback Stops
+        function DeferLoop()
+            if reaper.GetPlayState() == 0 or reaper.GetPlayPosition() > end_time then
+                reaper.SoloAllTracks(0)
+                reaper.OnStopButton()
+            else
+                reaper.defer(function() DeferLoop() end)
+            end
         end
+        DeferLoop()
         
-        reaper.SetMediaTrackInfo_Value(track, 'I_SOLO', 1)
-        
+    else
+        reaper.ShowMessageBox("Please select at least one item!", "Warning", 0)
     end
-
-    reaper.ShowConsoleMsg(tostring(end_time))
-
-    reaper.OnPlayButton()
-
-    function onCallBack()
-        if reaper.GetPlayState() == 0 or reaper.GetPlayPosition() > end_time then
-            reaper.SoloAllTracks(0)
-            reaper.OnStopButton()
-        else
-            reaper.defer(function() onCallBack() end)
-        end
-    end
-
-    onCallBack(call)
-else
-    reaper.ShowMessageBox("Please select at least one item!", "Warning", 0)
 end
+
+main()
