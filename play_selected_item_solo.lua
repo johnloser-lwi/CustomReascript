cb0a7a3445e34fc879c0fd059a4de61dc29b54c8
johnloser <john-masterchief@hotmail.com>
2021-09-14 13:38:30 +0800

    Add auto stop to play_selected_item_solo

--- play_selected_item_solo.lua
+++ play_selected_item_solo.lua
@@ -1,8 +1,12 @@
 
-if reaper.CountSelectedMediaItems(0) > 0 then
+local item_count = reaper.CountSelectedMediaItems(0)
+
+if item_count > 0 then
     reaper.SoloAllTracks(0)
 
-    for i = 0, reaper.CountSelectedMediaItems(0) - 1 do
+    local end_time = -1
+
+    for i = 0, item_count - 1 do
         local item = reaper.GetSelectedMediaItem(0, i)
         local track = reaper.GetMediaItem_Track(item)
         local  _,track_name = reaper.GetTrackName(track)
@@ -12,17 +16,22 @@ if reaper.CountSelectedMediaItems(0) > 0 then
             reaper.Main_OnCommand(41173, 1)
         end
 
+        if i == item_count -1 then
+            end_time = reaper.GetMediaItemInfo_Value(item, "D_POSITION") + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+        end
         
         reaper.SetMediaTrackInfo_Value(track, 'I_SOLO', 1)
         
     end
 
+    reaper.ShowConsoleMsg(tostring(end_time))
 
     reaper.OnPlayButton()
 
     function onCallBack()
-        if reaper.GetPlayState() == 0 then
+        if reaper.GetPlayState() == 0 or reaper.GetPlayPosition() > end_time then
             reaper.SoloAllTracks(0)
+            reaper.OnStopButton()
         else
             reaper.defer(function() onCallBack() end)
         end
