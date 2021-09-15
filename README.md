784eba8274abd62adf69075e28f2e844c6c92d94
johnloser <john-masterchief@hotmail.com>
2021-09-15 14:20:06 +0800

    Modify group_track

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Some of my custom reascripts to help my workflow, feel free to use them!
 * Solo all selected items' track
 * Playback (Auto stop when reaching the end of the last item)
 * When playback stops, unsolo all tracks
+
+### group_track
+Group tracks like in every other DAW, you know what I mean! SWS required!
--- group_track.lua
+++ group_track.lua
@@ -1,15 +1,12 @@
-track = reaper.GetSelectedTrack(0, 0)
-index = reaper.GetMediaTrackInfo_Value(track, 'IP_TRACKNUMBER')
+local track = reaper.GetSelectedTrack(0, 0)
+local index = reaper.GetMediaTrackInfo_Value(track, 'IP_TRACKNUMBER')
 reaper.InsertTrackAtIndex(index - 1, true)
-folder = reaper.GetTrack(0, index - 1)
+local folder = reaper.GetTrack(0, index - 1)
 
 reaper.GetSetMediaTrackInfo_String(folder, 'P_NAME', 'New Group', true)
 
-for i = 0, reaper.CountSelectedTracks(0) - 1 do
-    sel_track = reaper.GetSelectedTrack(0, i)
-    reaper.SetMediaTrackInfo_Value(folder, 'I_FOLDERDEPTH', reaper.GetMediaTrackInfo_Value(folder, 'I_FOLDERDEPTH') + 1)
+reaper.SetTrackSelected(folder, true)
 
-    if i == reaper.CountSelectedTracks(0) - 1 then
-        reaper.SetMediaTrackInfo_Value(sel_track, 'I_FOLDERDEPTH', reaper.GetMediaTrackInfo_Value(sel_track, 'I_FOLDERDEPTH') - 1)
-    end
-end
+reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_SELCHILDREN2'), 1)
+
+reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_MAKEFOLDER'), 1)
