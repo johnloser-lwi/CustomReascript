77537e71710aa34998a400a40e21d03c2139b5b1
johnloser <john-masterchief@hotmail.com>
2021-09-15 14:33:04 +0800

    group_track will now auto select new folder's name box

--- group_track.lua
+++ group_track.lua
@@ -3,10 +3,15 @@ local index = reaper.GetMediaTrackInfo_Value(track, 'IP_TRACKNUMBER')
 reaper.InsertTrackAtIndex(index - 1, true)
 local folder = reaper.GetTrack(0, index - 1)
 
-reaper.GetSetMediaTrackInfo_String(folder, 'P_NAME', 'New Group', true)
+
 
 reaper.SetTrackSelected(folder, true)
 
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_SELCHILDREN2'), 1)
 
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_MAKEFOLDER'), 1)
+
+reaper.GetSetMediaTrackInfo_String(folder, 'P_NAME', 'New Group', true)
+reaper.SetOnlyTrackSelected(folder)
+
+reaper.Main_OnCommand(40696, 1)
