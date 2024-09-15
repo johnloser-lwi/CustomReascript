df49e4ca43186617f913c585fb323a2e06ab08b3
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2024-09-15 13:00:19 -0700

    make sure working on the track under the mouse when doing trim and fade

--- split.lua
+++ split.lua
@@ -1,2 +1,3 @@
+reaper.Main_OnCommand(41110, 0) -- Track: Select track under mouse
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELITEMSUNDEDCURSELTX"), 0) -- Xenakios/SWS: Select items under edit cursor on selected tracks
 reaper.Main_OnCommand(40759, 0) -- Item: Split items at edit cursor (select right)
--- trimend.lua
+++ trimend.lua
@@ -1,2 +1,3 @@
+reaper.Main_OnCommand(41110, 0) -- Track: Select track under mouse
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELITEMSUNDEDCURSELTX"), 0) -- Xenakios/SWS: Select items under edit cursor on selected tracks
 reaper.Main_OnCommand(41311, 0) -- Item edit: Trim right edge of item to edit cursor
